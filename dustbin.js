8c67f6c5b687dd7f8d11bfbabb181130cf5dc8ea
Aditya-legend <67062121+Aditya-legend@users.noreply.github.com>
2020-07-13 19:16:14 +0530

    Add files via upload

---
+++ dustbin.js
@@ -0,0 +1,9 @@
+class DustBin{
+    costructor(){
+    function keyPressed(){
+    if(keyCode===UP_ARROW){
+    Matter.Body.applyForce(paperObject.body,paperObject.body.position,{x:85,y:-85})
+    }
+    }
+    }
+}
